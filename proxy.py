e42818938378903aedc2bee2d07a6dfbb05ba3aa
Zak Dasaro <zakdasaro@gmail.com>
2020-07-26 18:44:33 -0400

    Add logging.

--- proxy.py
+++ proxy.py
@@ -3,22 +3,29 @@ import subprocess
 import sys
 
 # Keeps track of whether or not the robot has crashed and needs to reset
-crashed = False
 state = {
     "crashed" : False,
-    "hasLimit" : False
+    "hasLimit" : False,
+    "hasOut" : False
 }
 
 movement_commands = ["moveForward", "turnLeft", "turnRight", "wallFront",
                      "wallLeft", "wallRight"]
 
+def write(stream, string):
+    stream.write(string)
+    stream.flush()
+
 def invalid_to_send(line):
     if state["crashed"]:
         # Commands to move, turn, or detect walls will be ignored.
         if line.split()[0] in movement_commands:
-            sys.stderr.write("Cannot move or detect walls while crashed. Must send ackReset.\n")
-            sys.stderr.flush()
-            return True
+            write(sys.stderr, "Cannot move or detect walls while crashed. Must send ackReset.\n")
+            return b"crash\n"
+    # getStat command cannot be sent
+    if line.split()[0] == "getStat":
+        write(sys.stderr, "Competition program cannot call getStat.\n")
+        return b"-1\n"
     return False
 
 commands_that_need_response = [
@@ -48,45 +55,65 @@ def is_movement_command(line):
 
 def get_distance_plus_turns():
     """Gets the total effective distance plus total turns from the simulator"""
-    sys.stdout.write("getStat total-effective-distance\n")
-    sys.stdout.flush()
+    write(sys.stdout, "getStat total-effective-distance\n")
     total_distance = int(sys.stdin.readline())
-    sys.stdout.write("getStat total-turns\n")
-    sys.stdout.flush()
+    write(sys.stdout, "getStat total-turns\n")
     total_turns = int(sys.stdin.readline())
     return total_distance + total_turns
+
+def write_all_stats(command, excepted=False):
+    with open(state["outfile"], "a") as f:
+        f.write("command: {}\n".format(command))
+        if excepted:
+            f.write("Exception occurred.\n")
+        # Print several important stats
+        stats_to_print = ["score",
+                        "best-run-distance",
+                        "best-run-effective-distance",
+                        "best-run-turns",
+                        "total-distance",
+                        "total-effective-distance",
+                        "total-turns"
+                        ]
+        for stat in stats_to_print:
+            write(sys.stdout, "getStat {}\n".format(stat))
+            value = sys.stdin.readline()
+            f.write("{}: {}".format(stat, value))
+        f.write("\n")
+
     
 def main():
     parser = argparse.ArgumentParser()
-    # parser.add_argument("--out", help="file to output the score")
+    parser.add_argument("--out", help="file to output the score")
     parser.add_argument("--limit", type=int, help="maximum number of turns plus movements before ending program")
-    # TODO Add feature to log score to output file once supported by the simulator API
-    # Uncomment those parser arguments once implemented.
 
     parser.add_argument("command", type=str, nargs="+", help="command that executes the maze-solving program")
     args = vars(parser.parse_args())
-    sys.stderr.write(str(args['command'])+'\n')
+    write(sys.stderr, " ".join(args['command'])+'\n')
 
     # Determine if a limit is invoked
     if args["limit"] is not None:
         state["hasLimit"] = True
         state["limit"] = args["limit"]
+    # Determine if an output log is requested
+    if args["out"] is not None:
+        state["hasOut"] = True
+        state["outfile"] = args["out"]
 
     # Launch the subprocess
     proc = subprocess.Popen(args['command'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         
     try:
         # Loop until subprocess terminates
-        # TODO If possible, add a time limit to terminate the program if it doesn't send a command after more than one second of waiting.
         for line in iter(proc.stdout.readline, b''):
             decode_line = line.decode('utf-8')
-            if invalid_to_send(decode_line):
+            response = invalid_to_send(decode_line)
+            if response:
                 # Send "crash" if mouse had previously crashed and has not reset.
-                proc.stdin.write(b"crash\n")
-                proc.stdin.flush()
+                # Send "-1" if getStat was called
+                write(proc.stdin, response)
                 continue
-            sys.stdout.write(decode_line)
-            sys.stdout.flush()
+            write(sys.stdout, decode_line)
             
             if (needs_response(decode_line)):
                 inp = sys.stdin.readline() # Read from simulator
@@ -95,23 +122,23 @@ def main():
                 elif (decode_line == "ackReset\n" and inp == "ack\n"):
                     # No longer crashed
                     state["crashed"] = False
-                proc.stdin.write(inp.encode('utf-8'))
-                proc.stdin.flush()
+                write(proc.stdin, inp.encode('utf-8'))
 
                 # Check if mouse exceeded limit of moves + turns
                 if (is_movement_command(decode_line) and state["hasLimit"]
                     and get_distance_plus_turns() > state["limit"]):
-                    sys.stderr.write("Maximum movements exceeded\n")
-                    sys.stderr.flush()
+                    write(sys.stderr, "Maximum movements exceeded\n")
                     proc.kill()
-                    exit(1)
+                    break
+        # Process is complete, log stats.
+        if state["hasOut"]:
+            write_all_stats(" ".join(args['command']))
     except:
-        sys.stderr.write("exception\n")
-        sys.stderr.flush()
+        write(sys.stderr, "exception\n")
+        if state["hasOut"]:
+            write_all_stats(" ".join(args['command']), excepted=True)
         proc.kill()
         raise
-    
-    # TODO(zdasaro) detect program crash and exit with error so the simulator know the program terminated unexpectedly.
 
 if __name__ == "__main__":
     main()
